ba4c39e708894ca9b120f56e99e6cb503c9eae5d
KinnarChowdhury1994 <kc1988@cse.jgec.ac.in>
2024-02-03 02:11:12 +0530

    pushed chages made in feature/circle-area that are retrieved from stash while stashing another files in feature/rectangle-are branch. Step 1

--- Q3.py
+++ Q3.py
@@ -15,9 +15,13 @@ class GeometryCalculator:
 if __name__ == "__main__":
     calculator = GeometryCalculator()
 
+
+''' CHECKING GIT STASH IN feature/circle-area '''
 # TODO: Implement the feature to calculate the area of a circle
 radius = 5
 print(f"The area of the circle with radius {radius} = {calculator.calculate_circle_area(radius)}")
+
+
 
 # TODO: Implement the feature to calculate the area of a rectangle # length = 10
 # width = 6
